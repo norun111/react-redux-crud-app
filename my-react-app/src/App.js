f9007401d5c733a42f618614ed6bb71e874b7fea
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-14 15:22:45 +0900

    Update App.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+// import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div><h1 >Hello World</h1></div>
-  );
+// function App() {
+//   return (
+//     <div><h1 >Hello World</h1></div>
+//   );
+// }
+
+class App extends Component {
+  render(){
+    return React.createElement(
+      "h1",
+      null,
+      "Hello, world!"
+    );
+  }
 }
 
 export default App;
