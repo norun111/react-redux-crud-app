22b21ef0ac8880a4362d134427fd6d34439ebde0
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-15 17:41:24 +0900

    Update index.js

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import reducers from './reducers'
+import reducers from './reducers';
 
 
 ReactDOM.render(
