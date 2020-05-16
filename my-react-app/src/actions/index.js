21ca7572b9024b634496f1539ab76a311ff8d9cb
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-16 18:27:01 +0900

    Update index.js

--- my-react-app/src/actions/index.js
+++ my-react-app/src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 export const READ_EVENTS = 'READ_EVENTS'
 export const CREATE_EVENT = 'CREATE_EVENT'
 export const DELETE_EVENT = 'DELETE_EVENT'
+export const READ_EVENT = 'READ_EVENT'
 
 
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
@@ -21,5 +22,10 @@ export const postEvent = values => async dispatch => {
 export const deleteEvent = id => async dispatch => {
   await axios.delete(`${ROOT_URL}/events/${id}${QUERYSTRING}`)
   dispatch({type: DELETE_EVENT, id })
-  console.log('delete');
+}
+
+export const getEvent = id => async dispatch => {
+  const response = await axios.get(`${ROOT_URL}/events/${id}${QUERYSTRING}`)
+  console.log(response);
+  dispatch({ type: READ_EVENT, response })
 }
