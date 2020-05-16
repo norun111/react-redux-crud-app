a706818b494ddec17c217d76282b24f098f70ae5
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-16 17:00:52 +0900

    Update index.js

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -10,10 +10,12 @@ import './index.css';
 
 import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new';
+import EventsShow from './components/events_show';
 import * as serviceWorker from './serviceWorker';
 import reducer from './reducers';
 
-const enhancer = process.env.NODE_ENV === 'development' ? composeWithDevTools(applyMiddleware( thunk )) : applyMiddleware( thunk )
+const enhancer = process.env.NODE_ENV === 'development' ? 
+  composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk)
 const store = createStore(reducer, enhancer)
 
 
@@ -21,8 +23,10 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <Switch>
-        <Route exact path="/events/new" component={EventsNew} />
-        <Route exact path="" component={EventsIndex} />
+        <Route path="/events/new" component={EventsNew} />
+        <Route path="/events/:id" component={EventsShow} />
+        <Route exact path="/" component={EventsIndex} />
+        <Route exact path="/events" component={EventsIndex} />
       </Switch>
     </BrowserRouter>
   </Provider>,
