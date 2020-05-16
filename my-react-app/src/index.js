f0f9a876dcb72475bf621ed1cc30f7803fdbb1bd
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-16 13:49:08 +0900

    Update index.js

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk'
 
 
 import './index.css';
@@ -11,7 +12,7 @@ import * as serviceWorker from './serviceWorker';
 import reducer from './reducers';
 
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware( thunk ))
 
 
 ReactDOM.render(
