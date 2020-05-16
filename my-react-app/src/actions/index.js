78def08d17df3aa24a8f74c1d26e207dc394c048
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-16 13:07:56 +0900

    Update index.js

--- my-react-app/src/actions/index.js
+++ my-react-app/src/actions/index.js
@@ -1,11 +1,5 @@
-export const INCREMENT = 'INCREMENT'
-export const DECREMENT = 'DECREMENT'
-
-export const increment = () => ({
-  type: 'INCREMENT'
-})
-
-export const decrement = () => ({
-  type: 'DECREAMENT'
+export const READ_EVENTS = 'READ_EVENTS'
+export const readEvents = () => ({
+  type: 'READ_EVENTS'
 })
 
