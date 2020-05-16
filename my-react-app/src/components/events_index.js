c6aaaac6f3b7251731bc7e7fa31b517a4dd76d47
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-16 13:01:09 +0900

    Create events_index.js

---
+++ my-react-app/src/components/events_index.js
@@ -0,0 +1,27 @@
+// import React from 'react';
+// import './App.css';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { increment, decrement } from '../actions'
+
+
+class EventsIndex extends Component{
+
+  render() {
+    const props = this.props
+
+    return(
+    <React.Fragment>
+      <div>value: { props.value }</div>
+      <button onClick={ props.increment }>+1</button>
+      <button onClick={ props.decrement }>-1</button>
+    </React.Fragment>
+    )
+  }
+}
+const mapStateToProps = state => ({ value: state.count.value })
+
+
+const mapDispatchToProps = ({ increment, decrement})
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex)
