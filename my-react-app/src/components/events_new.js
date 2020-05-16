d1dcf853f4e1e6d0accb019b52cd7446ff00a406
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-16 15:14:26 +0900

    Update events_new.js

--- my-react-app/src/components/events_new.js
+++ my-react-app/src/components/events_new.js
@@ -26,7 +26,7 @@ class EventsNew extends Component{
   }
 
   render() {
-    const { handleSubmit } = this.props
+    const { handleSubmit, pristine } = this.props
 
     return(
       <form onSubmit={ handleSubmit(this.onSubmit) }>
@@ -34,7 +34,7 @@ class EventsNew extends Component{
         <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
 
         <div>
-          <input type='submit' value="Submit" disabled={false} />
+          <input type='submit' value="Submit" disabled={pristine} />
           <Link to="/">Cancel</Link>
         </div>
       </form>
